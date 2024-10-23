70726d2d310dc95d6b6692f1a7ab96cc568dbc3c
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-23 23:23:21 +0200

    Test ok

--- src/main/java/jom/com/softserve/s5/task3/App.java
+++ src/main/java/jom/com/softserve/s5/task3/App.java
@@ -1,13 +1,20 @@
 package jom.com.softserve.s5.task3;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class App {
 
-	// Write your code here
+    public static final BinaryOperator<String> greetingOperator = (p1, p2) ->
+            "Hello " + p1 + " " + p2 + "!!!";
 
-	public static List<String> createGreetings(List<Person> people /* Second parameter here */) {
-		// Write your code here
-		return null;
-	}
+    public static List<String> createGreetings(List<Person> people, BinaryOperator<String> binaryOperator) {
+        List<String> greatingList = new ArrayList<>();
+        for (Person person : people) {
+            String great = binaryOperator.apply(person.name, person.surname);
+            greatingList.add(great);
+        }
+        return greatingList;
+    }
 }
