b16e5e65a531a4a02cfd9388656e804f2a6edbce
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-23 22:40:12 +0200

    Test ok

--- src/main/java/jom/com/softserve/s5/task2/App.java
+++ src/main/java/jom/com/softserve/s5/task2/App.java
@@ -1,10 +1,27 @@
 package jom.com.softserve.s5.task2;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class App {
-    public static double[] getChanged(
-            double[] initialArray
-            /*place the second parameter here*/    ) {
-    	//your code
-    	return null;
+    public  static final Consumer<double[]> cons = (double[] arr) -> {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 2) {
+                arr[i] *= 0.8;
+            } else {
+                arr[i] *= 0.9;
+            }
+        }
+    };
+
+
+
+    public static double[] getChanged(double[] initialArray, Consumer<double[]> consumer) {
+        double[] changedArray = new double[initialArray.length];
+        System.arraycopy(initialArray, 0, changedArray, 0, initialArray.length);
+
+        consumer.accept(changedArray);
+        return changedArray;
     }
 }
