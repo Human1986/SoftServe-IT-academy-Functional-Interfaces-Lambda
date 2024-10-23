b81285bc26aec54f8b72387dc9bdb6a381532f70
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-23 21:39:54 +0200

    Test ok

--- src/main/java/jom/com/softserve/s5/task1/MyUtils.java
+++ src/main/java/jom/com/softserve/s5/task1/MyUtils.java
@@ -1,8 +1,16 @@
 package jom.com.softserve.s5.task1;
 
+import java.util.function.Predicate;
+
 public class MyUtils {
-    public static int getCount( /* Place the parameters here */ ) {
-    	// Write your code here
-        return 0;
+    public static int getCount(int[] array, Predicate<Integer> predicate) {
+        int count = 0;
+        boolean testedElement;
+
+        for (int element : array) {
+            testedElement = predicate.test(element);
+            if (testedElement) count++;
+        }
+        return count;
     }
 }
